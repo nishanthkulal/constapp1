fe34decfb87781d2f9d99beaadad736637d3265d
nishanthkulal <nishathkulal65462@gmail.com>
2022-12-27 05:03:05 -0800

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// searchForm Section code
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+}
